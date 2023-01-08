299f2bdd16bf135747e9e91bc0f5d6638f597f4f
PedroNishimura <pedro.fnishimura@gmail.com>
2023-01-08 17:41:01 -0300

    v1.0.1

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,25 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Connection, getRepository } from 'typeorm';
+import { Connection, getRepository, Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>; //ENTENDER MELHOR ISSO
+
+const createMockRepository = <T = any> (): MockRepository<T> => ({
+  findOne: jest.fn()
+})
+
 describe('UsersService', () => {
   let service: UsersService;
+  let userRepository: MockRepository
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UsersService,
         {provide: Connection, useValue: {}},
-        {provide: getRepositoryToken(User), useValue: {}}
+        {provide: getRepositoryToken(User), useValue: createMockRepository()}
       ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    userRepository = module.get<MockRepository>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('userProfile', () => {
+    describe('Buscar usuário pelo ID', () => {
+      it('Deve retornar o objeto user', async () => {
+        const userId = '1';
+        const expectReturn = {};
+
+        userRepository.findOne.mockReturnValue(expectReturn)
+        const user = await service.userProfile(userId);
+        expect(user).toEqual(expectReturn);
+      });
+    });
   });
 });
