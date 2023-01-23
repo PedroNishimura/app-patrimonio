93f5e1260e7f5b4760eb1b073d21c57f7236b51f
PedroNishimura <pedro.fnishimura@gmail.com>
2023-01-23 20:57:00 -0300

    2.0

--- ormConfig.js
+++ ormConfig.js
@@ -1,12 +1,16 @@
+require('dotenv').config();
+
 module.exports = {
     type: 'mysql',
-    host: 'mysqldb',
-    port: 3306,
-    username: 'root',
-    password: 'mySqlAdm',
-    database: 'myWallet',
+    host: process.env.HOST_DOCKER,
+    port: process.env.PORT,
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
     entities: ['dist/**/*.entity.js'],
     migrations: ['dist/migrations/*.js'],
+    autoLoadEntities: true,
+    synchronize: true,
     cli: {
         migrationsDir: 'src/migrations'
     }
